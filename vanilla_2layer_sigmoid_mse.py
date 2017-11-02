fixed minibatch gradient descent

vanilla_2layer_sigmoid_mse.py
@@ -49,36 +49,26 @@ w1 = np.random.randn(a0.shape[1],y.shape[1])
 #
 #    
 # backprop + minibatch SGD
-for i in range(batch):
-    # shuffle for all training data
-    arr=np.random.permutation(a0.shape[0])
-    # apply shuffle for a0
-    a0=a0[arr]
-    # take the 0:minibatch
-    a0=a0[:minibatch]
-    # apply shuffle for y
-    y=y[arr]
-    # take the 0:minibatch
-    y=y[:minibatch]
-    
-    z1=np.dot(a0,w1)
-    a1 = sigmoid(z1)
-    err1 = ((a1-y)/minibatch)*sigmoid_grad(z1)
-    w1 -= alpha * np.dot(a0.T,err1)
+# backprop + minibatch SGD
+for iter in range(batch):
+    error = 0
+#    only use minibatch number of data 
+    for i in range(0,X.shape[0]//minibatch,minibatch):
+        batch_x=X[i:i+minibatch]
+        batch_y=y[i:i+minibatch]
+            
+        a0 = batch_x    
+        z1 = np.dot(a0,w1)
+        a1 = sigmoid(z1)
+
+        #update w2 first
+        err1 = ((a1-batch_y)/minibatch)*sigmoid(z1)
+        w1 -= alpha * np.dot(a0.T,err1)
+        
+        error += (np.sum(np.abs(err1)))
+    sys.stdout.write("\rIter:" + str(iter) + " Loss:" + str(error))
+    if(iter % 100 == 99):
+        print("")
     
 
-#a0 = X 
-#y_predict = np.dot(a0,w1)
-#
-#arr = np.arange(X.shape[0])
-#np.random.shuffle(arr)
-#a0=a0[arr]
-#a0=a0[:minibatch]
-#y=y[arr]
-#y=y[:minibatch]
-#    
-#z1=np.dot(a0,w1)
-#a1 = sigmoid(z1)
-#err1 = ((a1-y)/minibatch)*sigmoid_grad(z1)
-#w1 -= alpha * np.dot(a0.T,err1)
 

vanilla_3layer_relu_sigmoid_mse.py
@@ -1,21 +1,21 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Oct 31 07:36:30 2017
-
-@author: amajidsinar
-"""
 
 import numpy as np
+import pandas as pd
+import sys
 
+dataset = pd.read_csv('Dataset/fashion-mnist_train.csv').values
 
-X = np.array([  [0,0,1],
-                [0,1,1],
-                [1,0,1],
-                [1,1,1] ])
-    
-# output dataset            
-y = np.array([[0,1,1,0]]).T
+y = dataset[:,0]
+y = np.reshape(y, (len(y),1))
+X = dataset[:,1:]
+
+mask = (y == 0) | (y == 1)
+index = np.where(mask)
+index = index[0]
+
+y = y[index] / 255
+X = X[index] / 255
 
 def relu(z):
     return z * (z > 0)
@@ -33,40 +33,45 @@ def sigmoid_grad(z):
 np.random.seed(1)
 
 alpha = 0.01
-batch = 10000
-minibatch = 2
+batch = 1000
+minibatch = 64
 
 a0 = X 
 hidden_neuron = 5
 
 # make sure the mean of weight is close to zero and std is close to one -- a good practice according to Efficient Backprop paper
-w1 = np.random.randn(a0.shape[1],hidden_neuron)
-w2 = np.random.randn(hidden_neuron,y.shape[1])
+w1 = np.random.randn(784,hidden_neuron)
+w2 = np.random.randn(hidden_neuron,1)
 
 # backprop + minibatch SGD
-for i in range(batch):
-    #only use minibatch number of data 
-    shuffle = np.random.permutation(a0.shape[0])
-    a0=a0[shuffle]
-    a0=a0[:minibatch]
-    y=y[shuffle]
-    y=y[:minibatch]
-    
-    
-    z1 = np.dot(a0,w1)
-    a1 = sigmoid(z1)
-    z2 = np.dot(a1,w2)
-    a2 = sigmoid(z2)
-    
-    #update w2 first
-    err2 = ((a2-y)/minibatch)*sigmoid_grad(z2)
-    w2 -= alpha * np.dot(a1.T,err2)
-    
-    #update w1
-    err1 = np.dot(err2,w2.T)*sigmoid_grad(z1)
-    w1 -= alpha * np.dot(a0.T,err1)
-    
+for iter in range(batch):
+    error = 0
+#    only use minibatch number of data 
+    for i in range(0,X.shape[0]//minibatch,minibatch):
+        batch_x=X[i:i+minibatch]
+        batch_y=y[i:i+minibatch]
+            
+        a0 = batch_x    
+        z1 = np.dot(a0,w1)
+        a1 = sigmoid(z1)
+        z2 = np.dot(a1,w2)
+        a2 = sigmoid(z2)
+            
+            #update w2 first
+        err2 = ((a2-batch_y)/minibatch)*relu_grad(z2)
+        w2 -= alpha * np.dot(a1.T,err2)
+            
+            #update w1
+        err1 = np.dot(err2,w2.T)*sigmoid_grad(z1)
+        w1 -= alpha * np.dot(a0.T,err1)
+        
+        error += (np.sum(np.abs(err2)))
+    sys.stdout.write("\rIter:" + str(iter) + " Loss:" + str(error))
+    if(iter % 100 == 99):
+        print("")
 
+
+    
 
 
 